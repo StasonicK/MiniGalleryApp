43cd3fd1d39ff4217553011b10ae9f2b4b157a74
Stanislav Anishchenko <anischenko_stas@mail.ru>
2023-06-25 19:22:28 +0600

    Added percents into Loading_container.

--- Assets/CodeBase/Screens/Common/LoadingSlider.cs
+++ Assets/CodeBase/Screens/Common/LoadingSlider.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,6 +9,7 @@ namespace CodeBase.Screens.Common
     public class LoadingSlider : MonoBehaviour
     {
         [SerializeField] private Slider _slider;
+        [SerializeField] private TextMeshProUGUI _percentsText;
         [SerializeField] private float _loadingTime;
 
         private float _spentTime = 0f;
@@ -19,6 +22,9 @@ namespace CodeBase.Screens.Common
             while (_spentTime < _loadingTime)
             {
                 _slider.value = _spentTime / _loadingTime;
+                float rawPercents = _slider.value * 100f;
+                double percents = Math.Round(rawPercents, 0);
+                _percentsText.text = $"{percents}%";
                 _spentTime += Time.deltaTime;
                 yield return null;
             }
